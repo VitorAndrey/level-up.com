4ecf1b99546a2c33ab2a12d9e3a3f0deed76020f
Vitor Andrey <vitor.andreylopes@gmail.com>
2024-01-17 16:37:36 -0300

    feat: add image

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,10 @@
 import { ArrowLeftIcon } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 
+import image from "../../../public/login.png.jpg";
 import { LevelUpLogoWhite } from "@/components/ui/level-up-logo-white";
 import { authOptions } from "@/utils/auth";
 
@@ -20,7 +22,15 @@ export default async function AuthLayout({ children }: AuthLayoutProps) {
   return (
     <main className="container relative grid h-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-        <div className="absolute inset-0 bg-red-900" />
+        <Image
+          priority
+          alt="image"
+          src={image}
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="absolute inset-0 h-full w-full bg-[url('/login.png.jpg')] object-cover object-center brightness-50"
+        />
         <Link
           href={"/"}
           className="relative z-20 flex items-center gap-1 text-lg font-medium"
